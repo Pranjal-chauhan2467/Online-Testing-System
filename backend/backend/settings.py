cd4c32104ff66378cb63a987680adedfdd70cb27
Pranjal chauhan <pranjalchauhan05780@gmail.com>
2026-08-14 12:15:58 +0530

    Configure Render host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1",
-    "localhost",]
+    "localhost",
+ "online-testing-system-9wl9.onrender.com",
+ ]
 
 
 # Application definition
